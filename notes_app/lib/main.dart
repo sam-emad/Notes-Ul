4af45cb02fa9d8d7739254dced7cd13e01fd279a
samvictor-code <113173943+samvictor-code@users.noreply.github.com>
2023-01-12 23:04:38 +0000

    chang theme and create notes viwe

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_viwe.dart';
 
 void main() {
-  runApp(const NotesApp ());
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
@@ -9,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: NotesViwe (),
+    );
   }
 }
